0b6b500e1642184553772079db1a019201f78da1
Vincent Schauer <schauervincent@gmail.com>
2019-05-14 13:36:06 +0200

    doppelte email nicht möglich und behandelt

--- add_students.php
+++ add_students.php
@@ -1,42 +1,68 @@
 <?php
+
+var_dump($_POST);
+
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
+
 $email = strtolower($firstname . "." . $lastname . "@stud.fh-campuswien.ac.at");
-try {
-
-    $conn = new PDO("mysql:host=localhost; dbname=webtech", "oliver", "nlkj");
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-    $create= "CREATE TABLE IF NOT EXISTS `webtech`.`userss` 
+$conn = new PDO("mysql:host=localhost; dbname=webtech", "oliver", "nlkj");
+
+$query =  "SELECT count('email') FROM users WHERE email= :email LIMIT 1";
+
+// Check the email with database
+$stm = $conn->prepare($query);
+$stm->bindParam(':email', $email);
+$stm->execute();
+
+// Get the result
+$result = $stm->fetchColumn();
+
+// Check if result is greater than 0 - user exist
+if ($result == 1) {
+
+   $message = "Der Schüler mit der E-Mail existiert schon!";
+
+   echo "<script type='text/javascript'>alert('$message');</script>";
+
+   header("Location: teacher.php");
+}  
+ 
+   try {
+
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+      $create = "CREATE TABLE IF NOT EXISTS `webtech`.`users` 
     ( `id` INT(30) NOT NULL AUTO_INCREMENT , 
     `firstname` VARCHAR(50) NOT NULL , 
     `lastname` VARCHAR(50) NOT NULL , 
     `email` VARCHAR(100) NOT NULL ,
     PRIMARY KEY (`id`)) ENGINE = InnoDB";
 
-
-    // use exec() to create table because no results are returned
-    $conn->exec($create);
-    echo "Table users created successfully";
- }
-catch(PDOException $exception)
- {
- echo $create . "<br>" . $exception->getMessage();
- }
-
- 
-$statement = $conn->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$statement->execute(array('firstname' => $firstname, 'lastname' => $lastname, 'email' => $email));   
+      // use exec() to create table because no results are returned
+      $conn->exec($create);
+      echo "Table users created successfully";
+   } catch (PDOException $exception) {
+      echo $create . "<br>" . $exception->getMessage();
+   }
 
 
-$subject = 'project work';
-$message = 'You have been added as a student: please add your password following this link http://localhost:8081/semesterwork/add_password.php to set up your account';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-mail($email, $subject, $message, $headers);
+   $statement = $conn->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+   $statement->execute(array('firstname' => $firstname, 'lastname' => $lastname, 'email' => $email));
 
 
-header("Location: teacher.php");
+   $subject = 'project work';
+   $message = 'You have been added as a student: please add your password following this link http://localhost:8081/semesterwork/add_password.php to set up your account';
+   $headers = 'From: webmaster@example.com' . "\r\n" .
+      'Reply-To: webmaster@example.com' . "\r\n" .
+      'X-Mailer: PHP/' . phpversion();
+   mail($email, $subject, $message, $headers);
+
+
+   header("Location: teacher.php");
+
+
